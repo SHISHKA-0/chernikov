5e6f067f5ba73dc73f3e2670a50173bc0ea4482d
SHISHKA-0 <kosteachernikov@gmail.com>
2024-10-31 16:57:42 +0300

    4-3-pwm.py

---
+++ 4-3-pwm.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+dac=[8, 11, 7, 1, 0, 5, 12, 6]
+duty_cycle = 0
+t = 0
+T = 10
+try:
+    while true:
+        duty_cycle=float(input())
+        t = duty_cycle*T
+        GPIO.output(21, 3.3)
+        timesleep(t)
+        GPIO.output(21, 0)
+        timesleep(T)
